368b2b3cc8e5cf43a1da9717c29154679a956839
Vika Mikhlai <vikamikhlai@gmail.com>
2022-05-23 18:41:30 +0300

    feat: solved 2 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('process');
+const rl = readline.createInterface({ input, output });
+const fs = require('fs');
+const path = require('path');
+let data = '';
+
+function writeInput(input) {
+  if (input === 'exit') {
+    console.log('Bye!');
+    rl.close();
+  } else {
+    if (input) data += input  + '\n';
+    fs.writeFileSync(path.join(__dirname, 'text.txt'), data, 'utf-8',
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+}
+writeInput();
+rl.question('Write some text\n', writeInput);
+rl.on('line', writeInput);
+rl.on('SIGINT', () => {
+  console.log('Bye!');
+  rl.close();
+});
+
