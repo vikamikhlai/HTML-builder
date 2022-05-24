12b669a66babdad2a3174f238e081a9f63595751
Vika Mikhlai <vikamikhlai@gmail.com>
2022-05-24 03:01:53 +0300

    fix: change 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,14 +3,17 @@ const path = require('path');
 const folder = path.join(__dirname);
 const folderFiles = path.join(__dirname, 'files');
 const newFolderFiles = path.join(__dirname, 'files-copy/');
-fs.mkdir(folder + '/files-copy', { recursive: true }, (err) => {
-  if (err) throw err;
-});
 
-fs.readdir(folderFiles, (err, files) => {
-  files.forEach(file => {
-    fs.copyFile((folderFiles + '/' + file), (newFolderFiles + file), (err) => {
-      if (err) throw err;
+fs.rm(folder + '/files-copy', { recursive: true }, (err) => {
+  fs.mkdir(folder + '/files-copy', { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(folderFiles, (err, files) => {
+    files.forEach(file => {
+      fs.copyFile((folderFiles + '/' + file), (newFolderFiles + file), (err) => {
+        if (err) throw err;
+      });
     });
   });
 });
